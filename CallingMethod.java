2926d38ebf8593beabdaebd8567733272361b1ee
VishwaPravin <109669223+VishwaPravin@users.noreply.github.com>
2022-07-27 10:21:25 +0530

    Add files via upload

---
+++ CallingMethod.java
@@ -0,0 +1,15 @@
+package practicephase1;
+
+public class CallingMethod 
+{
+	static void myMethod() 
+	{
+        System.out.println("You have called me! My name is: myMethod!");
+    }
+
+    public static void main(String[] args)
+    {
+        myMethod();
+    }
+
+}
