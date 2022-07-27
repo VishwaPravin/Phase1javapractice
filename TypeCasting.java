2dde849e61b33946ff6553532ef50d3f23e4d4a5
VishwaPravin <109669223+VishwaPravin@users.noreply.github.com>
2022-07-27 10:23:06 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,15 @@
+package practicephase1;
+
+public class TypeCasting 
+{
+	public static void main(String[]args)
+	{
+	int x = 18;
+	System.out.println("Before conversation,int value "+x);
+	long y = x;
+	System.out.println("After conversation,long value "+y);
+	float z = y;
+	System.out.println("After conversation,float value "+z);
+	}
+
+}
